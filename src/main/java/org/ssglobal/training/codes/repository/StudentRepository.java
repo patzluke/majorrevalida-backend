cb663783425267a46aa48d408515af1ca3b25d79
Patrick Astrero <patzluke@gmail.com>
2023-06-20 17:05:55 +0800

    modified fixed the insert Into of student

--- src/main/java/org/ssglobal/training/codes/repository/StudentRepository.java
+++ src/main/java/org/ssglobal/training/codes/repository/StudentRepository.java
@@ -28,18 +28,23 @@ public class StudentRepository {
 
 	public Row15<Integer, Integer, Integer, String, String, String, String, LocalDate, Integer, Integer, String, String, Boolean, String, String> insertStudent(
 			Student student) {
-		return dslContext.insertInto(STUDENT).set(STUDENT.PASSWORD, student.getPassword())
-				.set(STUDENT.FIRST_NAME, student.getFirstName()).set(STUDENT.MIDDLE_NAME, student.getMiddleName())
-				.set(STUDENT.LAST_NAME, student.getLastName()).set(STUDENT.BIRTH_DATE, student.getBirthDate())
-				.set(STUDENT.SEM, student.getSem()).set(STUDENT.YEAR_LEVEL, student.getYearLevel())
-				.set(STUDENT.ACADEMIC_YEAR, student.getAcademicYear()).set(STUDENT.STATUS, student.getStatus())
-				.set(STUDENT.ACTIVE_DEACTIVE, student.getActiveDeactive()).set(STUDENT.COURSE, student.getCourse())
+		return dslContext.insertInto(STUDENT)
+				.set(STUDENT.PROGRAM_ID, student.getProgramId())
+				.set(STUDENT.PASSWORD, student.getPassword())
+				.set(STUDENT.FIRST_NAME, student.getFirstName())
+				.set(STUDENT.MIDDLE_NAME, student.getMiddleName())
+				.set(STUDENT.LAST_NAME, student.getLastName())
+				.set(STUDENT.BIRTH_DATE, student.getBirthDate())
+				.set(STUDENT.SEM, student.getSem())
+				.set(STUDENT.YEAR_LEVEL, student.getYearLevel())
+				.set(STUDENT.ACADEMIC_YEAR, student.getAcademicYear())
+				.set(STUDENT.STATUS, student.getStatus())
+				.set(STUDENT.ACTIVE_DEACTIVE, student.getActiveDeactive())
+				.set(STUDENT.COURSE, student.getCourse())
 				.set(STUDENT.IMAGE, student.getImage())
-				.returning(STUDENT.STUDENT_ID, STUDENT.STUDENT_NO, STUDENT.PROGRAM_ID, STUDENT.PASSWORD,
-						STUDENT.FIRST_NAME, STUDENT.MIDDLE_NAME, STUDENT.LAST_NAME, STUDENT.BIRTH_DATE, STUDENT.SEM,
-						STUDENT.YEAR_LEVEL, STUDENT.ACADEMIC_YEAR, STUDENT.STATUS, STUDENT.ACTIVE_DEACTIVE,
-						STUDENT.COURSE, STUDENT.IMAGE)
-				.fetchOne().valuesRow();
+				.returning()
+				.fetchOne()
+				.valuesRow();
 	}
 
 	public Row15<Integer, Integer, Integer, String, String, String, String, LocalDate, Integer, Integer, String, String, Boolean, String, String> updateStudent(
