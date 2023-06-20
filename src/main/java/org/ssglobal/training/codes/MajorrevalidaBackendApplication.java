56209abded81d07eec8e1dddfcc1ea4689e46fda
Patrick Astrero <patzluke@gmail.com>
2023-06-20 10:22:19 +0800

    first commit

---
+++ src/main/java/org/ssglobal/training/codes/MajorrevalidaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ssglobal.training.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorrevalidaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorrevalidaBackendApplication.class, args);
+	}
+
+}
