3b53abe43072c5fe970c0ee9cf3e0bb1280b424c
Matthew Adaro <117700776+MatthewAdaro@users.noreply.github.com>
2023-06-23 10:37:11 +0800

    Added view profile for student

--- src/main/java/org/ssglobal/training/codes/controller/StudentCapabilitiesController.java
+++ src/main/java/org/ssglobal/training/codes/controller/StudentCapabilitiesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +16,29 @@ import org.ssglobal.training.codes.service.StudentCapabilitiesService;
 @RestController
 @RequestMapping(value = "/api/student")
 public class StudentCapabilitiesController {
-	
+
 	@Autowired
 	private StudentCapabilitiesService service;
-		
-	@PutMapping(value = "/update/{studentId}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<UserAndStudent> updateStudent(@RequestBody UserAndStudent student, @PathVariable("studentId") Integer studentId){
+
+	@GetMapping(value = "/view/{studentId}", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserAndStudent> viewStudentProfile(@PathVariable("studentId") Integer studentId) {
+		try {
+			UserAndStudent studentProfile = service.viewStudentProfile(studentId);
+			if (studentProfile != null) {
+				return ResponseEntity.ok(studentProfile);
+			}
+		} catch (Exception e) {
+			System.out.println("%s".formatted(e));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.badRequest().build();
+	}
+
+	@PutMapping(value = "/update/{studentId}", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserAndStudent> updateStudent(@RequestBody UserAndStudent student,
+			@PathVariable("studentId") Integer studentId) {
 		try {
 			UserAndStudent updatedStudent = service.updateStudent(student, studentId);
 			if (updatedStudent != null) {
@@ -32,5 +50,5 @@ public class StudentCapabilitiesController {
 		}
 		return ResponseEntity.badRequest().build();
 	}
-	
-}	
+
+}
