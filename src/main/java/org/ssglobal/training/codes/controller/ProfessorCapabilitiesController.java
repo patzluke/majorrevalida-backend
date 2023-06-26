96650d39949ebf707d1d2f3b2dd3a95f92964b37
CDOMINS@win-wks301532 <christiandomins07@gmail.com>
2023-06-26 13:01:00 +0800

    Professor Attendance and Loads

--- src/main/java/org/ssglobal/training/codes/controller/ProfessorCapabilitiesController.java
+++ src/main/java/org/ssglobal/training/codes/controller/ProfessorCapabilitiesController.java
@@ -1,5 +1,8 @@
 package org.ssglobal.training.codes.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.ssglobal.training.codes.model.UserAndProfessor;
 import org.ssglobal.training.codes.service.ProfessorCapabilitiesService;
+import org.ssglobal.training.codes.tables.pojos.ProfessorLoad;
 
 @RestController
 @RequestMapping(value = "/api/professor")
@@ -41,6 +45,34 @@ public class ProfessorCapabilitiesController {
 			UserAndProfessor updatedProfessor = service.updateProfessor(userAndProfessor);
 			if (updatedProfessor != null) {
 				return ResponseEntity.ok(updatedProfessor);
+			}
+		} catch (Exception e) {
+			System.out.println("%s".formatted(e));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@GetMapping(value = "/get/loads/{professorNo}")
+	public ResponseEntity<List<ProfessorLoad>> selecAllLoad(Integer professorNo) {
+		try {
+			List<ProfessorLoad> selectedLoads = service.selectAllLoad(professorNo);
+			if (selectedLoads != null) {
+				return ResponseEntity.ok(selectedLoads);
+			}
+		} catch (Exception e) {
+			System.out.println("%s".formatted(e));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@GetMapping(value = "/get/loads/subject/{professorNo}")
+	public ResponseEntity<List<Map<String, Object>>> selecAllLoads(Integer professorNo) {
+		try {
+			List<Map<String, Object>> selectedLoads = service.selectAllLoads(professorNo);
+			if (selectedLoads != null) {
+				return ResponseEntity.ok(selectedLoads);
 			}
 		} catch (Exception e) {
 			System.out.println("%s".formatted(e));
